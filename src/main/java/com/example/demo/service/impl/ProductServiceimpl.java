a34fe60d414d37a4a9819e3102c2c63c96a211d4
bala-amypo <bala@amypo.in>
2025-12-19 04:13:56 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/ProductServiceimpl.java
@@ -0,0 +1,36 @@
+package com.example.demo.service.impl;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.demo.entity.Product;
+import com.example.demo.exception.BadRequestException;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.repository.ProductRepository;
+import com.example.demo.service.ProductService;
+
+@Service
+public class ProductServiceimpl implements ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Override
+    public Product createProduct(Product product) {
+        if (productRepository.findBySku(product.getSku()).isPresent()) {
+            throw new BadRequestException("SKU already exists");
+        }
+        return productRepository.save(product);
+    }
+
+    @Override
+    public Product getProductById(long id) {
+        return productRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Product not found"));
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+}
