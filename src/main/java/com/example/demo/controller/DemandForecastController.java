fad20f4af19aff4ca1a5ea0298a8c7b2c56b3079
bala-amypo <bala@amypo.in>
2025-12-19 04:25:56 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/DemandForecastController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.example.demo.entity.DemandForecast;
+import com.example.demo.service.DemandForecastService;
+
+@RestController
+@RequestMapping("/api/forecasts")
+public class DemandForecastController {
+
+    @Autowired
+    DemandForecastService demandForecastService;
+
+    @PostMapping
+    public DemandForecast createForecast(@RequestBody DemandForecast forecast) {
+        return demandForecastService.createForecast(forecast);
+    }
+
+    @GetMapping("/store/{storeId}/product/{productId}")
+    public DemandForecast getForecast(@PathVariable long storeId, @PathVariable long productId) {
+        return demandForecastService.getForecast(storeId, productId);
+    }
+}
