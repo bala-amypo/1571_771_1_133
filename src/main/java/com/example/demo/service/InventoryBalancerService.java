27e5e258b1a10cd6297c386e8efa08c7f3ab453c
bala-amypo <bala@amypo.in>
2025-12-19 10:07:18 +0000

    User commit

--- src/main/java/com/example/demo/service/InventoryBalancerService.java
+++ src/main/java/com/example/demo/service/InventoryBalancerService.java
@@ -1,12 +1,13 @@
 package com.example.demo.service;
 
 import java.util.List;
-
-import com.example.demo.entity.InventoryLevel;
 import com.example.demo.entity.TransferSuggestion;
 
 public interface InventoryBalancerService {
-    List<TransferSuggestion> generateSuggestions(long productId);
-    List<TransferSuggestion> getSuggestionsForStore(long storeId);
-    TransferSuggestion getSuggestionById(long id);
+
+    List<TransferSuggestion> generateSuggestions(Long productId);
+
+    List<TransferSuggestion> getSuggestionsForStore(Long storeId);
+
+    TransferSuggestion getSuggestionById(Long id);
 }
