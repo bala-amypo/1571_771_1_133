4d07b070ae8bc819cadf4b28a89d2e8bafab1902
bala-amypo <bala@amypo.in>
2025-12-19 05:07:57 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ProductServiceimpl.java
+++ src/main/java/com/example/demo/service/impl/ProductServiceimpl.java
@@ -1,45 +1,36 @@
 package com.example.demo.service.impl;
 
-import java.time.LocalDate;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.example.demo.entity.DemandForecast;
 import com.example.demo.entity.Product;
-import com.example.demo.entity.Store;
 import com.example.demo.exception.BadRequestException;
-import com.example.demo.repository.DemandForecastRepository;
-import com.example.demo.service.DemandForecastService;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.repository.ProductRepository;
 import com.example.demo.service.ProductService;
-import com.example.demo.service.StoreService;
 
 @Service
-public class DemandForecastServiceimpl implements DemandForecastService {
+public class ProductServiceimpl implements ProductService {
 
     @Autowired
-    private DemandForecastRepository demandForecastRepository;
-
-    @Autowired
-    private StoreService storeService;
-
-    @Autowired
-    private ProductService productService;
+    private ProductRepository productRepository;
 
     @Override
-    public DemandForecast createForecast(DemandForecast forecast) {
-        if (!forecast.getForecastDate().isAfter(LocalDate.now())) {
-            throw new IllegalArgumentException("Forecast date must be in the future");
+    public Product createProduct(Product product) {
+        if (productRepository.findBySku(product.getSku()).isPresent()) {
+            throw new BadRequestException("SKU already exists");
         }
-        if (forecast.getPredictedDemand() < 0) {
-            throw new IllegalArgumentException("Quantity must be >= 0");
-        }
-        return demandForecastRepository.save(forecast);
+        return productRepository.save(product);
     }
 
     @Override
-    public DemandForecast getForecast(long storeId, long productId) {
-        Store store = storeService.getStoreById(storeId);
-        Product product = productService.getProductById(productId);
-        return demandForecastRepository.findByStoreAndProductAndForecastDateAfter(store, product, LocalDate.now())
-                .orElseThrow(() -> new BadRequestException("No forecast found"));
+    public Product getProductById(long id) {
+        return productRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Product not found"));
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
     }
 }
