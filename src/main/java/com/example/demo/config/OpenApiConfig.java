ee8ee5d58aa95d0872d09e51975aa7096d35d09e
bala-amypo <bala@amypo.in>
2025-12-26 17:47:19 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,27 +1,41 @@
 package com.example.demo.config;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .name("Authorization");
+        
+       
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("bearerAuth");
+        
         return new OpenAPI()
-                .components(new Components()
-                        .addSecuritySchemes("bearerAuth",
-                                new SecurityScheme()
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")))
                 .info(new Info()
-                        .title("Demo API")
-                        .version("1.0.0")
-                        .description("API documentation"));
+                        .title("Inventory Balancer API")
+                        .version("1.0")
+                        .description("API for Multi-Location Inventory Balancer"))
+                .addSecurityItem(securityRequirement)
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("bearerAuth", securityScheme))
+                .servers(List.of(
+                        new Server().url("https://9095.pro604cr.amypo.ai")
+                ));
     }
 }
