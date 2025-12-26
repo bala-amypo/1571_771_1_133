dba6bccf8de7c33484ad6dc35363f51d8b904334
bala-amypo <bala@amypo.in>
2025-12-26 17:58:56 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -2,8 +2,6 @@ package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,25 +13,11 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
-        
-        SecurityScheme securityScheme = new SecurityScheme()
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT")
-                .name("Authorization");
-        
-       
-        SecurityRequirement securityRequirement = new SecurityRequirement()
-                .addList("bearerAuth");
-        
         return new OpenAPI()
                 .info(new Info()
                         .title("Inventory Balancer API")
                         .version("1.0")
                         .description("API for Multi-Location Inventory Balancer"))
-                .addSecurityItem(securityRequirement)
-                .components(new io.swagger.v3.oas.models.Components()
-                        .addSecuritySchemes("bearerAuth", securityScheme))
                 .servers(List.of(
                         new Server().url("https://9095.pro604cr.amypo.ai")
                 ));
